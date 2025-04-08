99eb3f04e9bfeb053642624b664303b6dbbfe572
Михаил <115717017+mykhail-druz@users.noreply.github.com>
2025-04-08 23:53:32 +0300

    Added vite for dev, added video in section, added all icons for simplify section

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+    root: 'src',
+    publicDir: 'public',
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+            },
+        },
+    },
+    server: {
+        open: true,
+        port: 3000,
+    },
+})
